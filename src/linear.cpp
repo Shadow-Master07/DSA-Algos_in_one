9d6bd513e9f1aa84318b4439f7b1da237afc2d81
Shadow Master <kpriyanshu01programmer@gmail.com>
2024-11-06 19:21:08 +0530

    Mostly completed linear.cpp file

--- src/linear.cpp
+++ src/linear.cpp
@@ -199,16 +199,86 @@ namespace linear
     class DoublyLinkedList
     {
         private:
-            D_Node *head;
+            D_Node *m_head;
         
         public:
-            DoublyLinkedList(int data);
-            void addData(int data);
-            void showData();
-            void deleteData(int data);
-            void searchData(int data);
+            DoublyLinkedList(int data)
+            {
+                m_head = (D_Node*) malloc(sizeof(D_Node));
+                m_head->next = nullptr;
+                m_head->data = data;
+                m_head->previous = nullptr;
+            }
+
+            void addData(int data)
+            {
+                D_Node *temp = (D_Node*) malloc(sizeof(D_Node));
+                temp->data = data;
+                temp->next = nullptr;
+                temp->previous = nullptr;
+
+                // Temporary place to point to by variable, that is to be used as a place where we store the final data
+                D_Node *temporary;
+                for (temporary = m_head; temporary->next != nullptr; temporary = temporary->next )
+                {}
+                // Storing the pointer to where we actually needed to
+                temporary->next = temp;
+                temp->previous = temporary;
+            }
+
+            void showData()
+            {
+                for (D_Node *temp = m_head; temp != nullptr; temp = temp->next)
+                {
+                    std::cout << temp->data << std::endl;
+                }
+            }
+
+            void deleteData(int data)
+            {
+                D_Node *temp, *oneStepBehind;
+                for (temp = m_head; (temp != nullptr) && (temp->data != data); temp->next)
+                {
+                    oneStepBehind = temp;
+                }
+
+                if (temp == nullptr)
+                {
+                    std::cout << "Data does not exist" << std::endl;
+                    return;
+                }
+                
+                if (temp->data = data)
+                {
+                    oneStepBehind->next = temp->next;
+                    temp->next->previous = oneStepBehind;
+                    free(temp);
+                    std::cout << "Data deleted" << std::endl;
+                    return;
+                }
+            }
+
+            void searchData(int data)
+            {
+                D_Node *temp;
+                for (temp = m_head; (temp != nullptr) && (temp->data != data); temp->next)
+                {}
+
+                if (temp == nullptr)
+                {
+                    std::cout << "Data does not exist" << std::endl;
+                    return;
+                }
+
+                if (temp->data = data)
+                {
+                    std::cout << "Data exists" << std::endl;
+                }
+            }
+
     };
 
+    // Incomplete
     class HashTable
     {};
 };
